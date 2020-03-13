de3b98961f448d8ce8f84c27ae89496551def0bd
NKBelousov <belousovnikita92@gmail.com>
2020-03-13 20:11:47 +0300

    setup side navigation layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+type Route = {
+  icon: string;
+  title: string;
+  href: string;
+};
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'expense-tracker-frontend';
+  title = "expense-tracker-frontend";
+
+  routes: Route[] = [
+    {
+      icon: "home",
+      title: "Home",
+      href: "/",
+    },
+    {
+      icon: "attach_money",
+      title: "Payments",
+      href: "/payments",
+    },
+    {
+      icon: "info",
+      title: "About",
+      href: "/about",
+    },
+    {
+      icon: "exit_to_app",
+      title: "Logout",
+      href: "/logout",
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatListModule } from "@angular/material/list";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
