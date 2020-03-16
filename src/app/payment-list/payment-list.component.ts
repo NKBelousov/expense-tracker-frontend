2f058d2ac134192fac5754e8ba61f13f33ef51e3
NKBelousov <belousovnikita92@gmail.com>
2020-03-16 20:13:50 +0300

    add basic list view for payments

--- src/app/payment-list/payment-list.component.ts
+++ src/app/payment-list/payment-list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-payment-list',
-  templateUrl: './payment-list.component.html',
-  styleUrls: ['./payment-list.component.scss']
+  selector: "app-payment-list",
+  templateUrl: "./payment-list.component.html",
+  styleUrls: ["./payment-list.component.scss"],
 })
 export class PaymentListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  items = [
+    {
+      id: 0,
+      user_id: 0,
+      type_id: 0,
+      name: "Payment",
+      cost: 42,
+      created_at: new Date().getTime(),
+    },
+  ];
+
+  onCreate(): void {
+    console.log("create");
   }
-
 }
